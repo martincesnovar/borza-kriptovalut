31bd9bd4f4a00b24c8383a510211077fc8b9f2cb
Martin Češnovar <martin.cesnovar@gmail.com>
2018-02-11 22:08:27 +0100

    popravki priprava tabel.py

--- priprava tabel.py
+++ priprava tabel.py
@@ -1,18 +1,37 @@
 import sqlite3, os
 IME_BAZE='Kriptovalute.db'
-con = sqlite3.connect(IME_BAZE)
-cur = con.cursor()
+
+def ustvari_bazo(ime_baze):
+    """ create a database connection to a SQLite database """
+    try:
+        conn = sqlite3.connect(ime_baze)
+    except Error as e:
+        pass
+    finally:
+        conn.close()
 
 def sestavi_bazo(ime_baze=IME_BAZE,brisi=False):
-    if brisi and os.path.exists(ime_baze):
-        os.remove(ime_baze)
-    dodaj_osebo()
-    dodaj_valute()
-    dodaj_lastnistvo_valut()
-    dodaj_zgodovino()
+    try:
+        con = sqlite3.connect(ime_baze)
+        cur = con.cursor()
+        if brisi and os.path.exists(ime_baze):
+            os.remove(ime_baze)
+        ustvari_bazo(ime_baze)
+        dodaj_osebo(ime_baze)
+        dodaj_valute(ime_baze)
+        dodaj_lastnistvo_valut(ime_baze)
+        dodaj_zgodovino(ime_baze)
+    except Exception as e:
+        raise e
+    finally:
+        con.close()
+    return True
+    
 
 
-def dodaj_osebo():
+def dodaj_osebo(ime_baze):
+    con = sqlite3.connect(ime_baze)
+    cur = con.cursor()
     sql = '''CREATE TABLE Oseba (
         id      INTEGER PRIMARY KEY AUTOINCREMENT
                         NOT NULL
@@ -26,38 +45,43 @@ def dodaj_osebo():
     cur.execute(sql)
 
 
-def dodaj_valute():
+def dodaj_valute(ime_baze):
+    con = sqlite3.connect(ime_baze)
+    cur = con.cursor()
     sql='''CREATE TABLE Valuta (
-    id  INTEGER PRIMARY KEY AUTOINCREMENT
+    id  CHAR PRIMARY KEY 
                 NOT NULL
                 UNIQUE,
-    ime         NOT NULL
-                UNIQUE
+    ime CHAR    NOT NULL
     );'''
     cur.execute(sql)
 
 
-def dodaj_zgodovino():
+def dodaj_zgodovino(ime_baze):
+    con = sqlite3.connect(ime_baze)
+    cur = con.cursor()
     sql = '''CREATE TABLE Zgodovina (
-    Oseba             REFERENCES Oseba (id) 
+    Oseba    INTEGER  REFERENCES Oseba (id) 
                       NOT NULL,
-    Valuta            REFERENCES Valuta (id) 
+    Valuta   CHAR     REFERENCES Valuta (id) 
                       NOT NULL,
-    Datum    DATETIME DEFAULT (datetime('now') ) 
-                      NOT NULL,
-    kolicina INTEGER
+    kolicina DECIMAL  NOT NULL,
+    cena     DECIMAL  NOT NULL,
+    datum    DATETIME DEFAULT (datetime('now') ) 
+                      NOT NULL
     );'''
     cur.execute(sql)
 
-def dodaj_lastnistvo_valut():
+def dodaj_lastnistvo_valut(ime_baze):
+    con = sqlite3.connect(ime_baze)
+    cur = con.cursor()
     sql = '''CREATE TABLE lastnistvo_valut (
     lastnik  INTEGER  REFERENCES Oseba (id) 
                       NOT NULL,
-    valuta   INTEGER  NOT NULL
-                      UNIQUE
+    valuta   CHAR  NOT NULL
                       REFERENCES Valuta (id),
     vrednost DECIMAL  NOT NULL,
-    kolicina INTEGER  NOT NULL,
+    kolicina DECIMAL  NOT NULL,
     Datum    DATETIME NOT NULL
                       DEFAULT (datetime('now') ) 
     );'''
